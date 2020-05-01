20881af63f4a6acf991178c9ea22e65a6a2ae607
Sydney Krantz <syd107@gmail.com>
2020-05-01 15:25:47 -0700

    created server that runs and model for workouts and seeded db

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+// app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
+
+// routes
+    // GET route to get last workout /api/workouts/
+    // PUT route to add exercise to existing workouts /api/workouts/
+    // POST route to create workout /api/workouts/
+    // GET? route for /api/workouts/range
+
+// 
+
+
